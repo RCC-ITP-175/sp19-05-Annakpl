91650912ab099077a5c8e5a7d1c7b2e2958f61f4
Michael Greene <mgreene@rappahannock.edu>
2019-04-21 10:22:02 -0400

    Add isntructor feedback and rename to .js

---
+++ sushi.js
@@ -0,0 +1,34 @@
+var name = "Anna ";
+
+name += "Perry";
+
+Var nameLength = name.length;
+///////////////////////
+// INSTRUCTOR FEEDBACK
+///////////////////////
+On line 5 var needs to be all lowercase
+
+var shinigami = [ichigo , gin , shunsui];
+///////////////////////
+// INSTRUCTOR FEEDBACK
+///////////////////////
+On line 11 the strings in your array should be wraped in quotes
+["ichigo", "gin", "shunsui"]
+
+shinigami push.(Kyoraku);
+///////////////////////
+// INSTRUCTOR FEEDBACK
+///////////////////////
+On line 18 your . is in the wrong place and your missing quotes around the string parameter
+shinigami.push("Kyoraku")
+shinigami is an array and .push is a function you can use on that array ("Kyoraku") is the
+value you want to push onto the end of that array.
+
+var substitute[0];
+
+shinigami.pop();
+///////////////////////
+// INSTRUCTOR FEEDBACK
+///////////////////////
+On line 29 .pop will remove the last element in the array "Kyoraku". Use .unshift to remove the first element
+in the array
